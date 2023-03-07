8117843ad5ac58af3855b494e71be96edced22fa
Vojtik1 <it2126@sspu-opava.cz>
2023-03-07 10:17:46 +0100

    04 - index.html

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-/* Připojení modulu frameworku Express */
-const express = require("express");
+
 /* Vytvoření základního objektu serverové aplikace */
+/* Připojení modulu frameworku Express (https://expressjs.com/) */
+const express = require("express");
+/* Připojení externího modulu body-parser (https://www.npmjs.com/package/body-parser) -
+middleware pro parsování těla požadavku */
+const bodyParser = require("body-parser");
+/* Připojení externího modulu moment (https://momentjs.com/) - knihovna pro formátování
+datových a časových údajů */
+const moment = require("moment");
+/* Připojení vestavěných modulů fs (práce se soubory) a path (cesty v adresářové struktuře) */
+const fs = require("fs");
+const path = require("path");
 const app = express();
 /* Nastavení porstarttu, na němž bude spuštěný server naslouchat */
 const port = 3000;
